fa67bbf9931795fd030edf8e273d5d76f12ad262
theMightyT <halvas_trevor@halvas.ca>
2020-07-31 13:48:52 -0400

    adding greensock

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 (() => {
   let audio = document.querySelector('audio'),
-      playButton = document.querySelector('.controls button');
+      playButton = document.querySelector('.controls button'),
+      typeAnim = gsap.timeline({paused: true});
 
-  function playAudio() {
-    document.querySelectorAll('path').forEach(path => path.classList.add('text-anim'));
-    document.querySelector("#typelayout").classList.add('container-turn');
-    document.querySelector(".tony-vector").classList.add('zoom-tony');
+  typeAnim.to("#you", { opacity: 1, duration: 0.2 })
+          .to("#wanna", { opacity: 1, duration: 0.25 })
+          .to("#play", { opacity: 1, duration: 0.25 })
+          .to("#rough", { opacity: 1, duration: 0.25 })
+          .to("#typelayout", { opacity: 1, rotate: -90, scaleX: 1.5, scaleY: 1.5, x: -60, y: -300, duration: 0.25}, "<");
 
+  function playAnimation() {
+    typeAnim.play();
     audio.play();
   }
 
-
-  playButton.addEventListener("click", playAudio);
-
+  playButton.addEventListener("click", playAnimation);
 })();
